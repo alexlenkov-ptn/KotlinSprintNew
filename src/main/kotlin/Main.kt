0ba4799c2831d5881e4472b9afd1a649155b7d4e
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-05-01 09:54:38 +0300

    L1-T5 solution

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+
+
+fun main() {
+    var seconds = 6480
+    val hourInSpace = "01"
+    var minutesInSpace = "48"
+    var secondsInSpace = "00"
+    print(hourInSpace)
+    print(":")
+    print(minutesInSpace)
+    print(":")
+    print(secondsInSpace)
+}
