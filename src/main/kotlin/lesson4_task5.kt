5d4b0780ef7e8f4541f59c9b95c210853628e5b0
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-09 14:08:00 +0300

    L4-T5 corrected solution

--- src/main/kotlin/lesson4_task5.kt
+++ src/main/kotlin/lesson4_task5.kt
@@ -1,37 +1,38 @@
+const val RECOMMEND_DAMAGE = false
+const val RECOMMEND_COUNT_CREW_MIN = 55
+const val RECOMMEND_COUNT_CREW_MAX = 70
+const val RECOMMEND_BOXES = 50
+const val RECOMMEND_WEATHER_IS_GOOD = false
 
+const val ALTERNATIVE_DAMAGE = true
+const val ALTERNATIVE_COUNT_CREW = 70
+const val ALTERNATIVE_BOXES = 50
+const val ALTERNATIVE_WEATHER_IS_GOOD = true
 
 fun main() {
-    val recommendDamage = false
-    val recommendCountCrewMin = 55
-    val recommendCountCrewMax = 70
-    val recommendBoxes = 50
-    val recommendWeatherIsGood = false
-
-    val alternativeDamage = true
-    val alternativeCountCrew = 70
-    val alternativeBoxes = 50
-    val alternativeWeatherIsGood = true
-
     val realDamage = true
     val realCountCrew = 70
     val realBoxes = 50
     val realWeatherIsGood = true
 
-    println("Корабль может отправиться в плавание: " +
-            "${ (recommendDamage == realDamage // корабль не имеет повреждений
-                    && (recommendCountCrewMin <= realCountCrew || recommendCountCrewMax >= realCountCrew)
-                    //число экипажа составляет от 55 до 70 человек (включительно)
-                    && recommendBoxes < realBoxes
-                    // на борту есть более 50 ящиков провизии
-                    && (recommendWeatherIsGood == realWeatherIsGood || recommendWeatherIsGood != realWeatherIsGood))
-                    // погода благоприятная или неблагоприятная
-                    
-                    || (alternativeDamage == realDamage // наличие незначительных повреждений
-                    && alternativeCountCrew == realCountCrew // на борту рекомендованный состав экипажа - 70 человек;
-                    && alternativeWeatherIsGood == realWeatherIsGood // погода благоприятна
-                    && alternativeBoxes <= realBoxes) //  на борту есть 50 и более ящиков провизии
-            }")
+    println(
+        "Корабль может отправиться в плавание: " +
+                "${
+                    (RECOMMEND_DAMAGE == realDamage // корабль не имеет повреждений
+                            && (RECOMMEND_COUNT_CREW_MIN <= realCountCrew || RECOMMEND_COUNT_CREW_MAX >= realCountCrew) &&
+                            //число экипажа составляет от 55 до 70 человек (включительно)
+                            RECOMMEND_BOXES < realBoxes &&
+                            // на борту есть более 50 ящиков провизии
+                            (RECOMMEND_WEATHER_IS_GOOD == realWeatherIsGood || 
+                                    RECOMMEND_WEATHER_IS_GOOD != realWeatherIsGood)) ||
+                            // погода благоприятная или неблагоприятная
 
+                            (ALTERNATIVE_DAMAGE == realDamage &&  // наличие незначительных повреждений
+                            ALTERNATIVE_COUNT_CREW == realCountCrew &&  // на борту рекомендованный состав экипажа - 70 человек;
+                            ALTERNATIVE_WEATHER_IS_GOOD == realWeatherIsGood &&  // погода благоприятна
+                            ALTERNATIVE_BOXES <= realBoxes) //  на борту есть 50 и более ящиков провизии
+                }"
+    )
 }
 
 /*
