582c7a62c5335c47b5451ae1cc93b75a41540445
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-12 23:43:56 +0300

    L8-T3 solution

---
+++ src/main/kotlin/lesson8_task3.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val arrayIngredients = arrayOf(POTATO, TOMATO, CARROT, BEEF, SALT, CPICES)
+
+    println("Напишите какой ингредиент вы хотите найти:")
+    val userIngredient = readln().toString().toLowerCase()
+
+    if (arrayIngredients.indexOf(userIngredient) >= 0) {
+        println("Ингредиент $userIngredient есть в таблице")
+    } else {
+        println("Такого ингредиента нет в таблице")
+    }
+}
+
+const val POTATO = "картошка"
+const val TOMATO = "помидор"
+const val CARROT = "морковь"
+const val BEEF = "говядина"
+const val SALT = "соль"
+const val CPICES = "специи"
