34eb9c770746b1a39f03c0098019a08d3409e576
Alex Lenkov <@>
2024-05-14 21:34:29 +0300

    L9-T3 corrected solution

--- src/main/kotlin/lesson9_task3.kt
+++ src/main/kotlin/lesson9_task3.kt
@@ -1,13 +1,18 @@
 fun main() {
-    val listOfIgredients = listOf(COUNTS_OF_EGGS, COUNTS_OF_MILK, COUNTS_OF_BUTTER)
+    val listOfIgredientsDefault = mutableListOf(COUNTS_OF_EGGS, COUNTS_OF_MILK, COUNTS_OF_BUTTER)
     var numberOfServing: Int
 
     println("Укажите количество порций для омлета:")
     numberOfServing = readln().toInt()
+    val listOfIgredientsNew = listOfIgredientsDefault.map {
+        it * numberOfServing
+    }
 
-    println("На $numberOfServing кол-во порций вам понадобится: " +
-            "Яиц - ${listOfIgredients[0] * numberOfServing} шт., молока - ${listOfIgredients[1] * numberOfServing} мл., " +
-            "сливочного масла - ${listOfIgredients[2] * numberOfServing} гр.")
+    println(
+        "На $numberOfServing кол-во порций вам понадобится: " +
+                "Яиц - ${listOfIgredientsNew[0]} шт., молока - ${listOfIgredientsNew[1]} мл., " +
+                "сливочного масла - ${listOfIgredientsNew[2]} гр."
+    )
 }
 
 const val COUNTS_OF_EGGS = 2
