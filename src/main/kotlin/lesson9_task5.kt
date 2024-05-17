316da0a29c96cba9e3fd97c25152dbf21daa2f43
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-17 09:32:08 +0300

    L9-T5 delete text

--- src/main/kotlin/lesson9_task5.kt
+++ src/main/kotlin/lesson9_task5.kt
@@ -1,44 +1,35 @@
 fun main() {
     val countIngredients = 1..5
     var userIngredient: String = ""
-    val mutableListOfIngredients: MutableList<String> = mutableListOf()
+    var mutableListOfIngredients: MutableList<String> = mutableListOf()
 
     for (i in countIngredients) {
         if (mutableListOfIngredients.isEmpty()) {
             println("Укажите ингредиент №$i")
             userIngredient = readln().toString().toLowerCase()
             mutableListOfIngredients.add(userIngredient)
-        }else{
+        } else {
             while (true) {
                 println("Укажите ингредиент №$i")
                 userIngredient = readln().toString().toLowerCase()
                 if (mutableListOfIngredients.contains(userIngredient)) {
                     println("Ингредиент $userIngredient уже был. Введите, пожалуйста, другой")
                     continue
-                }else {
+                } else {
                     mutableListOfIngredients.add(userIngredient)
                     break
                 }
             }
         }
     }
-    mutableListOfIngredients.sorted()
-    mutableListOfIngredients[NULL_POSITION].capitalize() // ВОТ тут нужно разобраться непонятно почему не тянется
+
+
+    mutableListOfIngredients = mutableListOfIngredients.sorted().toMutableList()
+
+    mutableListOfIngredients.set(NULL_POSITION, mutableListOfIngredients[NULL_POSITION].capitalize())
 
     println(mutableListOfIngredients)
 
 }
 
 const val NULL_POSITION = 0
-
-/*
-Задача 5* к Уроку 9
-
-Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
-
-- результат должен исключать повторы;
-- список должен выводиться в алфавитном порядке;
-
-- все элементы списка выведены в одну строку, разделяя слова запятыми;
-- первый элемент должен быть распечатан с заглавной буквы.
- */
