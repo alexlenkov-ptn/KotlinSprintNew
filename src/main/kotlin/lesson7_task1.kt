d55043bfa957fbbb3c0b20ded97dc45d9056459b
Alex Lenkov <@>
2024-05-17 23:15:45 +0300

    L7-T1 delete text and corrected solution

--- src/main/kotlin/lesson7_task1.kt
+++ src/main/kotlin/lesson7_task1.kt
@@ -2,9 +2,9 @@ fun main() {
     val rangeInt = 0..9
     val rangeChar = 'a'..'z'
 
-    for (i in RANGE_MIN..RANGE_MAX) {
-        val randomCountFor = (0..1).random()
-        if (randomCountFor == 0) {
+    for (i in RANGE_PASSWORD_MIN..RANGE_PASSWORD_MAX) {
+        val randomCountFor = (NULL..ONE).random()
+        if (randomCountFor == NULL) {
             print(rangeInt.random())
         } else {
             print(rangeChar.random())
@@ -12,5 +12,8 @@ fun main() {
     }
 }
 
-const val RANGE_MIN = 0
-const val RANGE_MAX = 5
+const val RANGE_PASSWORD_MIN = 0
+const val RANGE_PASSWORD_MAX = 5
+
+const val NULL = 0
+const val ONE = 1
