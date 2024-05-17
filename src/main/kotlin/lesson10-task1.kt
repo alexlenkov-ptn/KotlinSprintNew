82f5a5344ee4cab0aaee766beba0618280d3c898
Alex Lenkov <@>
2024-05-17 23:10:37 +0300

    L10-T1 solution

---
+++ src/main/kotlin/lesson10-task1.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val computerThrow = throwDice()
+    val peopleThrow = throwDice()
+    println("Компьютер бросил кубик. Выпало $computerThrow")
+    println("Человек бросил кубик. Выпало $peopleThrow")
+    if (computerThrow > peopleThrow) {
+        println("Победили машины")
+    }else if (computerThrow == peopleThrow) {
+        println("Боевая ничья")
+    }else {
+        println("Победило человечество")
+    }
+}
+
+fun throwDice() = (MIN_COUNT_DICE..MAX_COUNT_DICE).random()
+
+const val MIN_COUNT_DICE = 1
+const val MAX_COUNT_DICE = 6
