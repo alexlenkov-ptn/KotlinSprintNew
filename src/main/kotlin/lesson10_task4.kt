b54c5022a269ff96e2ec9e70ea5a030f7cb7a813
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-19 19:56:22 +0300

    L8-T4

---
+++ src/main/kotlin/lesson10_task4.kt
@@ -0,0 +1,56 @@
+
+fun main() {
+    var humanResultDice: Int
+    var computerResultDice: Int
+    var userAnswer: String = ""
+    var computerWins: Int = 0
+    var humanWins: Int = 0
+
+    do {
+        humanResultDice = generateDice()
+        computerResultDice = generateDice()
+        startRound(humanResultDice, computerResultDice)
+
+        userAnswer = readln().toLowerCase()
+
+        if(humanResultDice > computerResultDice) humanWins++
+        else if (humanResultDice < computerResultDice) computerWins++
+
+    }while (userAnswer == userPositiveAnswer)
+
+    println("Игра закончилась со счётом: \n" +
+            "Человечество: $humanWins \n" +
+            "Компьютеры: $computerWins")
+}
+
+fun startRound(humanResultDice: Int?, computerResultDice: Int?) {
+    println("Человечество сделало свой ход. Выпало число: $humanResultDice \n" +
+            "Машины сделали свой ход. Выпало число: $computerResultDice \n" +
+            "Хотите продолжить раунд? Да / Нет")
+}
+
+fun generateDice(): Int {
+    return (MIN_RESULT_DICE..MAX_RESULT_DICE).random().toInt()
+}
+
+const val MIN_RESULT_DICE = 1
+const val MAX_RESULT_DICE = 6
+const val userPositiveAnswer = "да"
+const val HALF_SECOND: Long = 500
+
+
+/*
+Задача 4 к Уроку 10
+
+Усовершенствуй игру, где пользователь и компьютер по очереди бросают кости.
+Побеждает выбросивший наибольшее число. Теперь дай возможность пользователю играть до тех пор пока ему не надоест.
+В конце программа должна вывести сколько партий он выиграл.
+
+- после первого раунда программа задает вопрос: “Хотите бросить кости еще раз? Введите Да или Нет”;
+- программа в зависимости от ответа запускает новый раунд или заканчивает игру с выводом количества выигрышных партий человека;
+- в программе должно быть минимум 2 метода (для проведения раунда и для генерации значений брошенных кубиков);
+- основная логика программы должна находиться в main(), а 2 другие функции будут вспомогательными.
+ */
+
+
+
