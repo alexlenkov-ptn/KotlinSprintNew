51c737bc23d22f6e41cc9f3cc81e22aad0722bbc
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-19 23:34:30 +0300

    L7-T5 corrected

--- src/main/kotlin/lesson7_task5.kt
+++ src/main/kotlin/lesson7_task5.kt
@@ -2,29 +2,31 @@ fun main() {
     val rangeUpperCaseLetter = 'A'..'Z'
     val rangeLowerCaseLetter = 'a'..'z'
     val rangeCounts = 0..9
-    val rangeOptionsPassword = 0..2
-
+    val rangeAll = (rangeCounts.toList() + rangeUpperCaseLetter + rangeLowerCaseLetter).toList()
+    val password: MutableList<String> = mutableListOf()
 
     do {
-        println("Задайте длину пароля: ")
+        println("Задайте длину пароля 6 и более символов: ")
         val lengthPassword = readln().toInt()
-            if(lengthPassword < MIN_LENGTH_PASSWORD) {
-                println("Ошибка. Укажите длину пароля 6 и более символов")
-            }else {
-                for (i in RANGE_MIN..lengthPassword) {
-                    val optionsPassword = rangeOptionsPassword.random()
-                    if (optionsPassword == 0) {
-                        print(rangeUpperCaseLetter.random())
-                    }else if (optionsPassword == 1) {
-                        print(rangeLowerCaseLetter.random())
-                    }else {
-                        print(rangeCounts.random())
-                    }
-                }
-                break
-            }
+        if (lengthPassword < MIN_LENGTH_PASSWORD) {
+            println("Ошибка. Укажите длину пароля 6 и более символов")
+            continue
+        }
+
+        password.add(0, rangeUpperCaseLetter.random().toString())
+        password.add(1, rangeLowerCaseLetter.random().toString())
+        password.add(2, rangeCounts.random().toString())
+
+        for (i in 3..lengthPassword - 1) {
+            password.add(rangeAll.random().toString())
+        }
     }while (lengthPassword <= MIN_LENGTH_PASSWORD)
+
+    password.shuffle()
+
+    val userPassword = password.map {
+        print(it)
+    }
 }
 
 const val MIN_LENGTH_PASSWORD = 6
-const val RANGE_MIN = 0
