d477131539bd037ac282709d1edd3b60fc75b44e
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-22 08:55:24 +0300

    L11-T2 solution

---
+++ src/main/kotlin/lesson11_task2/User2.kt
@@ -0,0 +1,37 @@
+package lesson11_task2
+
+class User2(
+        val id: Int,
+        val login: String,
+        val mail: String,
+        var password: String,
+        var bio: String,
+    ) {
+    fun showInformation(id: Int?, login: String?, mail: String?, password: String?, bio: String?) {
+        println(
+            "Id: $id \n" +
+                    "Login: $login \n" +
+                    "Mail: $mail \n" +
+                    "Password: $password \n" +
+                    "Bio: $bio"
+        )
+    }
+
+    fun setBio() {
+        println("Введите новое био")
+        val newBio = readln().toString()
+        bio = newBio
+    }
+
+    fun changePassword() {
+        println("Укажите текущий пароль:")
+        val userPassword = readln().toString()
+        if (userPassword == password) {
+            println("Укажите новый пароль")
+            val userPasswordNew = readln().toString()
+            password = userPasswordNew
+        } else {
+            println("Пароль неверный")
+        }
+    }
+}
---
+++ src/main/kotlin/lesson11_task2/main.kt
@@ -0,0 +1,19 @@
+package lesson11_task2
+
+fun main() {
+    val user1: User2 = User2(
+        id = 1,
+        login = "pavel",
+        mail = "pavel@mail.ru",
+        password = "0000",
+        bio = "Владислав. Живу в Краснодаре. 23 года"
+    )
+
+
+    user1.setBio()
+
+    user1.changePassword()
+
+    user1.showInformation(user1.id, user1.login, user1.mail, user1.password, user1.bio)
+
+}
