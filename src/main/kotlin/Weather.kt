f720c7e4ec677f471d5c6bda5f9b11711b360d9f
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-26 00:14:22 +0300

    L12-T1

---
+++ src/main/kotlin/Weather.kt
@@ -0,0 +1,13 @@
+
+
+class Weather() {
+    var dayTemperature: Int = 0
+    var nightTemperature: Int = 0
+    var isRain: Boolean = false
+
+    fun printWeather() {
+        println("дневная температура: $dayTemperature \n" +
+                "ночная температура: $nightTemperature \n" +
+                "наличие осадков в течение суток: $isRain")
+    }
+}
