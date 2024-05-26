0ec9baf54b9df5204556662037111bd6fa5e0981
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-26 19:39:14 +0300

    L12-T3

---
+++ src/main/kotlin/lesson12_task3/Weather.kt
@@ -0,0 +1,17 @@
+package lesson12_task3
+
+class Weather (val dayTemperatureKelvin: Int,
+               val nightTemperatureKelvin: Int,
+               val isRain: Boolean,) {
+
+    val dayTemperatureCelsium: Int = dayTemperatureKelvin - KELVIN_TO_CELSIUM.toInt()
+    val nightTemperatureCelsium: Int = nightTemperatureKelvin - KELVIN_TO_CELSIUM.toInt()
+
+    fun printWeather() {
+        println("дневная температура в цельсиях: $dayTemperatureCelsium \n" +
+                "ночная температура в цельсиях: $nightTemperatureCelsium \n" +
+                "наличие осадков в течение суток: $isRain")
+    }
+}
+
+const val KELVIN_TO_CELSIUM = 273.15
---
+++ src/main/kotlin/lesson12_task3/main.kt
@@ -0,0 +1,6 @@
+package lesson12_task3
+
+fun main() {
+    val weather = Weather(300, 280, true)
+    weather.printWeather()
+}
