531d059e9aeb73777788e48f97f4a5c41a1a8535
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-26 20:16:03 +0300

    L9-T5 corrected solution

--- src/main/kotlin/lesson9_task5.kt
+++ src/main/kotlin/lesson9_task5.kt
@@ -1,33 +1,30 @@
+import java.util.SortedSet
+
 fun main() {
     val countIngredients = 1..5
     var userIngredient: String = ""
-    var mutableSetOfIngredients: MutableSet<String> = mutableSetOf()
-    var listOfIngredients: List<String>
-    var firstElement: String = ""
-
+    val sortedSetOfIngredients: SortedSet<String> = sortedSetOf()
+    var result: String
         for (i in countIngredients) {
-        if (mutableSetOfIngredients.isEmpty()) {
+        if (sortedSetOfIngredients.isEmpty()) {
             println("Укажите ингредиент №$i")
             userIngredient = readln().toString().toLowerCase()
-            mutableSetOfIngredients.add(userIngredient)
+            sortedSetOfIngredients.add(userIngredient)
         } else {
             while (i in countIngredients) {
                 println("Укажите ингредиент №$i")
                 userIngredient = readln().toString().toLowerCase()
-                if (mutableSetOfIngredients.contains(userIngredient)) {
+                if (sortedSetOfIngredients.contains(userIngredient)) {
                     println("Ингредиент $userIngredient уже был. Введите, пожалуйста, другой")
                     continue
                 } else {
-                    mutableSetOfIngredients.add(userIngredient)
+                    sortedSetOfIngredients.add(userIngredient)
                     break
                 }
             }
         }
     }
-    mutableSetOfIngredients = mutableSetOfIngredients.sorted().toMutableSet()
-    // mutableSetOfIngredients.set(NULL_POSITION, mutableSetOfIngredients[NULL_POSITION].capitalize())
-    println(mutableSetOfIngredients)
+    result = sortedSetOfIngredients.joinToString(", ", "","").capitalize()
+    println(result)
 }
-
-const val NULL_POSITION = 0
 
