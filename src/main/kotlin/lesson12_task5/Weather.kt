1b3253a0240581aa8e130f7e811eead45db2c17c
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-27 17:42:06 +0300

    L12-T5

---
+++ src/main/kotlin/lesson12_task5/Weather.kt
@@ -0,0 +1,11 @@
+package lesson12_task5
+
+class Weather (_dayTemperature: Int,
+               _nightTemperature: Int,
+               _isRain: Boolean,) {
+
+    val dayTemperature = _dayTemperature
+    val nightTemperature = _nightTemperature
+    val isRain = _isRain
+
+}
