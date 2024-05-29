28385c6c38c64204dbb30e58dc6a9fc8206e0f8e
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-29 14:43:03 +0300

    L13-T3

--- src/main/kotlin/lesson13_task3/lesson13_task3.kt
+++ src/main/kotlin/lesson13_task3/lesson13_task3.kt
@@ -3,14 +3,13 @@ package lesson13_task3
 fun main() {
     val person1 = TelephoneList("Vitaly", 89991599999,)
     val person2 = TelephoneList("Pavel", 89991599992,)
-    val person3 = TelephoneList("Pavel", 89991599993,)
-    val person4 = TelephoneList("Pavel", 89991599994,)
-    val person5 = TelephoneList("Pavel", 89991599995,)
+    val person3 = TelephoneList("Pavel", 89991599993, "null")
+    val person4 = TelephoneList("Pavel", 89991599994, "Amazon")
+    val person5 = TelephoneList("Pavel", 89991599995, "Google")
 
+    val listOfContact: List<TelephoneList> = listOf(person1, person2, person3, person4, person5)
 
-
-    val listOfContact: List<TelephoneList> = listOf(person1)
-
-
-    person1.printInfo()
+    listOfContact.forEach {
+        println(it.company)
+    }
 }
