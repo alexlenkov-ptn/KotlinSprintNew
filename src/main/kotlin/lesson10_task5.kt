fe42488729e440a4c0f143c64529a814a3e4760b
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-30 13:04:25 +0300

    L10-T5 corrected solution

--- src/main/kotlin/lesson10_task5.kt
+++ src/main/kotlin/lesson10_task5.kt
@@ -2,11 +2,11 @@ fun main() {
     val login = readln().toString()
     val password = readln().toString()
     val userCard = listOf("Чеснок", "Огурцы", "Тесто", "Йогурт", "Хлеб")
-    val token = getToken()
+    val token = logIn(login, password)
 
     if (token != null) {
         println(returnCard(token))
-    }else {
+    } else {
         println("Данные для входа не совпадают")
     }
 }
@@ -17,27 +17,17 @@ fun returnCard(token: String?): List<String> {
 }
 
 fun logIn(login: String?, password: String?): String? {
-    if (login ==  USER_LOGIN && password == USER_PASSWORD) return getToken()
+    if (login == USER_LOGIN && password == USER_PASSWORD) return getToken()
     else return null
 }
 
 fun getToken(): String {
     val rangeTokenNumbers = 1..9
     val rangeTokenLetters = ('a'..'z')
-    val randomRangeCounts = 0..2
-    var randomCounts = 0
+    val randomRange = rangeTokenLetters + rangeTokenLetters
     var userToken: String = ""
 
-    for (i in 0..TOKEN_SYMBOLS) {
-        randomCounts = randomRangeCounts.random()
-        if (randomCounts == 0) {
-            userToken += rangeTokenNumbers.random()
-        } else if (randomCounts == 1) {
-            userToken += rangeTokenLetters.random().toLowerCase()
-        }else {
-            userToken += rangeTokenLetters.random().toUpperCase()
-        }
-    }
+    for (i in 0..TOKEN_SYMBOLS) userToken += randomRange.random()
     return userToken
 }
 
