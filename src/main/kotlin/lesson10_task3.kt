2f3f288c6c9d48e6054d99594cb887082d93a6c1
Alex Lenkov <alex.lenkov123@outlook.com>
2024-05-30 12:42:41 +0300

    L10-T3 corrected solution

--- src/main/kotlin/lesson10_task3.kt
+++ src/main/kotlin/lesson10_task3.kt
@@ -6,19 +6,18 @@ fun main() {
 
 }
 
-fun generatePassword(passwordSize: Int?): String { // Явно указали, что функция возвращает String
+fun generatePassword(passwordSize: Int): String { // Явно указали, что функция возвращает String
 
-    val listOfSpecialCymbol = listOf("!", "\"", "#", "\\", "\$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/")
-    val rangeListOfSpecialCymbol = COUNT_NULL..(listOfSpecialCymbol.size - 1)
+    val rangeOfSpecialCymbol = charArrayOf('!', '\'', '#', '\\', '\$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/')
     val rangePassword = 0..9
 
     var password: String = ""
 
-    for (i in COUNT_ONE..passwordSize!!) {
+    for (i in COUNT_ONE..passwordSize) {
         if (i % 2 == 0) {
             password += rangePassword.random().toString()
         } else {
-            password += listOfSpecialCymbol.get(rangeListOfSpecialCymbol.random())
+            password += rangeOfSpecialCymbol.random()
         }
     }
 
