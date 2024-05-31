b421b0f6329c7d7fd7bdf96e27059b9101b50928
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-01 00:58:59 +0300

    L12-T2 corrected solution

--- src/main/kotlin/lesson12_task2/Weather.kt
+++ src/main/kotlin/lesson12_task2/Weather.kt
@@ -1,13 +1,10 @@
 package lesson12_task2
 
     class Weather(
-        _dayTemperature: Int,
-        _nightTemperature: Int,
-        _isRain: Boolean,
+        var dayTemperature: Int,
+        var nightTemperature: Int,
+        var isRain: Boolean,
         ) {
-        var dayTemperature: Int = _dayTemperature
-        var nightTemperature: Int = _nightTemperature
-        var isRain: Boolean = _isRain
 
         fun printWeather() {
             println("дневная температура: $dayTemperature \n" +
