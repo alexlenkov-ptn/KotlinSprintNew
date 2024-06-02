9497508674328b6c683e80d09421dbfd45028fdd
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-03 00:35:12 +0300

    L13-T1 corrected solution

--- src/main/kotlin/lesson13_task1/lesson13_task1.kt
+++ src/main/kotlin/lesson13_task1/lesson13_task1.kt
@@ -3,3 +3,13 @@ package lesson13_task1
 fun main() {
     val vitaly = TelephoneList("vitaly", "test", null)
 }
+
+class TelephoneList(
+    _name: String,
+    _company: String,
+    _number: Long?,
+) {
+    val name = _name
+    val number = _number
+    val company = _company
+}
