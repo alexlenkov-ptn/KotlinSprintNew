624fca1850693ddaadcb68fbbd51ab0c35f88d1e
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-04 19:11:29 +0300

    L14-T2 corrected solution

--- src/main/kotlin/lesson14_task2/lesson14_task2.kt
+++ src/main/kotlin/lesson14_task2/lesson14_task2.kt
@@ -19,17 +19,12 @@ fun main() {
 }
 
 open class Liner_14_2(
-    // Поля по умолчанию
     var name: String = "Liner",
     var speed: Int = 10,
     var loadCapacity: Int = 20,
     var passengerCapacity: Int = 50,
     var areBreakIce: Boolean = false,
 ) {
-    // speed
-    // loadCapacity - грузоподъёмность
-    // passengerCapacity - вместительность пассажиров
-    // areBreakIce - ломают лёд (true / false)
     open fun loadCargo() {
         println("Горизонтальный трап со шкафута выдвинут")
     }
