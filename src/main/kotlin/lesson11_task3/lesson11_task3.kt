dd6ac11ced163e5c457da3dd3f806be278a27345
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-04 16:21:56 +0300

    L11-T3 corrected solution

--- src/main/kotlin/lesson11_task3/lesson11_task3.kt
+++ src/main/kotlin/lesson11_task3/lesson11_task3.kt
@@ -1,27 +1,27 @@
 package lesson11_task3
 
 fun main() {
-    val user1: User = User (
+    val user1: User = User(
         name = "Pavel",
     )
-    val user2: User = User (
+    val user2: User = User(
         name = "Viktor",
     )
-    val user3: User = User (
-        name = "Viktor",
+    val user3: User = User(
+        name = "Grigory",
     )
 
     val room1: Room = Room(
         cover = "green",
         name = "room1",
-        listOfUsers = mutableListOf(),
+        listOfUsers = mutableSetOf(),
     )
 
     room1.addUser(user1)
     room1.addUser(user2)
     room1.addUser(user3)
 
-    room1.updateUserStatus(user1, "микрофон заглушен")
+    room1.updateUserStatus("Pavel", STATUS_TALKING)
 }
 
 class User(
@@ -33,7 +33,7 @@ class User(
 class Room(
     val cover: String,
     val name: String,
-    var listOfUsers: MutableList<User> = mutableListOf(),
+    var listOfUsers: MutableSet<User> = mutableSetOf(),
 ) {
 
     fun addUser(user: User) {
@@ -41,18 +41,35 @@ class Room(
         else listOfUsers.add(user)
     }
 
-    fun updateUserStatus(user: User, status: String) {
-        val statusLowerCase = status.toLowerCase()
-        if (statusLowerCase == statusMuted ||
-            statusLowerCase == statusTalking ||
-            statusLowerCase == statusMicrophoneOff)
-            user.status = status
-        else
-            println("Такого статуса не существует")
+    fun updateUserStatus(userName: String, status: String) {
+        // Проверить что пользователь есть в списке - готово
+        // Создать поиск по имени, а не ссылке
+
+        val listOfUserName: Map<String, User> = listOfUsers.associateBy { it.name }
+
+        if (listOfUserName.contains(userName)) {
+
+            val statusLowerCase = status.toLowerCase()
+
+            if (statusLowerCase == STATUS_MUTED ||
+                statusLowerCase == STATUS_TALKING ||
+                statusLowerCase == STATUS_MICROPHONE_OFF
+            ) {
+
+                val user: User? = listOfUserName[userName]
+                user?.status = statusLowerCase
+            } else {
+                println("Такого статуса не существует")
+            }
+        } else {
+            println("Пользователя с таким именем не существует")
+        }
+
+
     }
 }
 
-const val statusMuted = "заглушен"
-const val statusTalking = "разговаривает"
-const val statusMicrophoneOff = "микрофон заглушен"
+const val STATUS_MUTED = "пользователь заглушен"
+const val STATUS_TALKING = "разговаривает"
+const val STATUS_MICROPHONE_OFF = "микрофон выключен"
 
