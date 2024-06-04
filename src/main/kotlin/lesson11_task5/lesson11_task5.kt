dbddadeb9da25d09ff389b6f13e182cf3568b829
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-05 00:26:47 +0300

    L11-T5 corrected

--- src/main/kotlin/lesson11_task5/lesson11_task5.kt
+++ src/main/kotlin/lesson11_task5/lesson11_task5.kt
@@ -3,23 +3,19 @@ package lesson11_task5
 fun main() {
     val forum = Forum()
     val factory: ForumPersonFactory = ForumPersonFactory()
+    val user1 = factory.createNewUser(1, "Dmitry")
+    val user2 = factory.createNewUser(2, "Pavel")
+    val message1 = factory.createNewMessage(1, "I am Dmitry")
+    val message2 = factory.createNewMessage(2, "I am Pavel")
+    forum.addUserForum(user1)
+    forum.addUserForum(user2)
+    forum.addMessageForum(message1)
+    forum.addMessageForum(message2)
 
-    factory.createNewUser(1, "Dmitry")
-    factory.createNewUser(2, "Pavel")
-    factory.createNewMessage(1, "I am Dmitry")
-    factory.createNewMessage(2, "I am Pavel")
-
-//todo Указал что нужно доделать ниже
-
-//    val user1 = createNewUser(1, "Dmitry")
-//    val user2 = forumPersonFactory.createNewUser(2, "Pavel")
-//    val message1 = forumPersonFactory.createNewMessage(2, "I am Pavel")
-//    val message2 = forumPersonFactory.createNewMessage(1, "I am Dmitry")
     forum.printThread()
 }
 
 class ForumPersonFactory {
-
     fun createNewUser(userId: Int, userName: String): ForumPerson {
         val user = ForumPerson(userId, userName)
         return user
@@ -43,37 +39,16 @@ class ForumMessage(
 
 class Forum() {
     val forumPersonFactory = ForumPersonFactory()
-
     val ListOfForumUsers: MutableSet<ForumPerson> = mutableSetOf()
     val ListOfForumMessage: MutableList<ForumMessage> = mutableListOf()
-
-    //todo Нужно реализовать функцию через айдишник, а не по ссылке
-
 
     fun addUserForum(user: ForumPerson) {
         ListOfForumUsers.add(user)
     }
-
     fun addMessageForum(message: ForumMessage) {
         ListOfForumMessage.add(message)
     }
-
     fun printThread() {
-//        for (i in 0 until  ListOfUsers.size) {
-//            val userName = ListOfUsers[i].userName
-//            val userId = ListOfUsers[i].userId
-//
-//            for (i in 0 until ListOfMessage.size) {
-//                val authorId = ListOfMessage[i].authorId
-//                val message = ListOfMessage[i].message
-//
-//                if (userId == authorId) {
-//                    println("автор: $userName")
-//                    println("сообщение: $message")
-//                }
-//            }
-//
-//        }
         ListOfForumUsers.forEach() {
             println("автор: ${it.userName}")
         }
