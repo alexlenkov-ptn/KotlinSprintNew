437799f7b49a62e49ecc448bca230c265c9710d1
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-05 01:12:45 +0300

    L14-T3 corrected solution

--- src/main/kotlin/lesson14_task3/lesson14_task3.kt
+++ src/main/kotlin/lesson14_task3/lesson14_task3.kt
@@ -1,20 +1,66 @@
 package lesson14_task3
 
-fun main() {
-    val blackCircle: Circle_14_3 = Circle_14_3("black", 14.0)
-    val whiteCircle: Circle_14_3 = Circle_14_3("white", 28.0)
+const val PI = 3.14
+const val BLACK = "black"
+const val WHITE = "white"
 
-    val blackRectangle: Rectangle_14_3 = Rectangle_14_3("black", 5.0, 10.0)
-    val whiteRectangle: Rectangle_14_3 = Rectangle_14_3("white", 16.0, 20.0)
+fun main() {
+    val blackCircle: Circle_14_3 = Circle_14_3(BLACK, 14.0)
+    val whiteCircle: Circle_14_3 = Circle_14_3(WHITE, 28.0)
+
+    val blackRectangle: Rectangle_14_3 = Rectangle_14_3(BLACK, 5.0, 10.0)
+    val whiteRectangle: Rectangle_14_3 = Rectangle_14_3(WHITE, 16.0, 20.0)
 
     val list: List<Figure_14_3> = listOf(blackCircle, whiteCircle, blackRectangle, whiteRectangle)
 
-    // todo
-    // Нужно как-то сделать это
-    // Создай несколько объектов разных цветов (черные и белые) и сохрани в единый список. Выведи в консоль:
-    //
-    //- сумму периметров всех черных фигур;
-    //- сумму площадей всех белых фигур.
+    var sumOfPerimeterBlackFigures: Double = 0.0
+    var sumOfAreaWhiteFigures: Double = 0.0
 
+    list.forEach() {
+        if (it.color == BLACK) {
+            sumOfPerimeterBlackFigures += it.calculatePerimeter()
+        }
+        if (it.color == WHITE) {
+            sumOfAreaWhiteFigures += it.calculateArea()
+        }
+    }
 
+    println(sumOfPerimeterBlackFigures)
+    println(sumOfAreaWhiteFigures)
+}
+
+abstract class Figure_14_3(
+    val color: String
+) {
+    abstract fun calculateArea(): Double
+    abstract fun calculatePerimeter(): Double
+}
+
+class Rectangle_14_3(
+    color: String,
+    val width: Double,
+    val height: Double,
+) : Figure_14_3(color = color) {
+
+    override fun calculateArea(): Double {
+        return width * height
+    }
+
+    override fun calculatePerimeter(): Double {
+        return 2 * (width + height)
+    }
+}
+
+class Circle_14_3(
+    color: String,
+    val radius: Double,
+) : Figure_14_3(color = color) {
+
+    override fun calculateArea(): Double {
+        return PI * Math.pow(radius, 2.0)
+    }
+
+    override fun calculatePerimeter(): Double {
+        return 2 * PI * radius
+    }
 }
