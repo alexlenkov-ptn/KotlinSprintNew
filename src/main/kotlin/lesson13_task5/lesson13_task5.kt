64c43fb89cc45a8d19c0df7f74662b88cd140c6d
Alex Lenkov <@>
2024-06-05 13:10:24 +0300

    L13-T5 corrected

--- src/main/kotlin/lesson13_task5/lesson13_task5.kt
+++ src/main/kotlin/lesson13_task5/lesson13_task5.kt
@@ -2,7 +2,7 @@ package lesson13_task5
 
 fun main() {
     var userAnswer: String = ""
-    val listOfContact: List<TelephoneList> = listOf()
+    val listOfContact: List<TelephoneList_13_5> = listOf()
 
     println("Введите номер телефона:")
     val number = readln().toString()
@@ -13,9 +13,27 @@ fun main() {
     }
 
     println("Введите имя:")
-    val name = readln().toString().capitalize()
+    val name = readln().toString().replaceFirstChar { it.uppercase() }
     number.toLong()
 
     println("Введите название компании:")
     val company = readln()?.toString() ?: null
+}
+
+class TelephoneList_13_5(
+    _name: String,
+    _number: Long,
+    _company: String? = null,
+) {
+    val name = _name
+    val number = _number
+    val company = _company ?: "не указано"
+
+    fun printInfo() {
+        println(
+            "- Имя: $name \n" +
+                    "- Номер: $number \n" +
+                    "- Компания: $company"
+        )
+    }
 }
