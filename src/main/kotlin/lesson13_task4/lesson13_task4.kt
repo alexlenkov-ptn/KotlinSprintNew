7d6c90bcfe9b21f9f651b00ac287c09b04b2a6c5
Alex Lenkov <@>
2024-06-05 13:22:28 +0300

    L13-T4 corrected

--- src/main/kotlin/lesson13_task4/lesson13_task4.kt
+++ src/main/kotlin/lesson13_task4/lesson13_task4.kt
@@ -1,15 +1,17 @@
 package lesson13_task4
 
+const val ANSWER_NO = "нет"
+
 fun main() {
     var userAnswer: String = ""
-    val listOfContact: List<TelephoneList> = listOf()
+    val listOfContact: List<TelephoneList_13_4> = listOf()
 
     while (userAnswer != ANSWER_NO) {
         println("Введите номер телефона:")
         val name = readln().toString().capitalize()
         val number = readln().toLongOrNull() ?: println("Вы не ввели номер телефона")
         val company = readln()?.toString() ?: null
-        val user: TelephoneList
+        val user: TelephoneList_13_4
         println("Продолжить заполнение базы? Да / Нет")
         userAnswer = readln().toString().toLowerCase()
     }
@@ -19,4 +21,20 @@ fun main() {
     }
 }
 
-val ANSWER_NO = "нет"
+class TelephoneList_13_4(
+    _name: String,
+    _number: Long,
+    _company: String? = null,
+) {
+    val name = _name
+    val number = _number
+    val company = _company ?: "не указано"
+
+    fun printInfo() {
+        println(
+            "- Имя: $name \n" +
+                    "- Номер: $number \n" +
+                    "- Компания: $company"
+        )
+    }
+}
