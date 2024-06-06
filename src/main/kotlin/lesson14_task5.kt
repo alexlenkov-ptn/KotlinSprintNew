289930b77c36769150f26f0ee32979c7773d871b
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-06 19:39:05 +0300

    L14 T5 solution

--- src/main/kotlin/lesson14_task5.kt
+++ src/main/kotlin/lesson14_task5.kt
@@ -1,17 +1,79 @@
+import kotlin.random.Random
 
 fun main() {
+    val chat = Chat()
+    chat.addMessage("Привет, я Valery", "Valery")
+
+    chat.addThreadMessage(
+        chat.mutableListOfMessages[0].messageId,
+        "Это комментарий",
+        "Valery"
+    )
+
+    chat.addThreadMessage(
+        chat.mutableListOfMessages[0].messageId,
+        "Это комментарий 2",
+        "Valery 2"
+    )
+
+    chat.addMessage("Привет, я Alex", "Alex")
+
+    chat.addThreadMessage(
+        chat.mutableListOfMessages[1].messageId,
+        "Hello, Alex",
+        "Pavel"
+    )
+
+    chat.addThreadMessage(
+        3,
+        "Это комментарий 3",
+        "Valery 3"
+    )
+
+    chat.printChat(chat)
 
 }
 
 class Chat() {
+    val mutableListOfMessages: MutableList<Message> = mutableListOf()
+    val mutableListOfThreadMessage: MutableList<ChildMessage> = mutableListOf()
 
-
-    fun addMessage() {
-        //  добавляет обычное сообщение в чат, принимает на вход текст и автора сообщения;
+    fun addMessage(text: String, authorName: String) {
+        val message = Message(messageId = Random.nextInt(0, Int.MAX_VALUE), message = text, authorName = authorName)
+        mutableListOfMessages.add(message)
     }
 
-    fun addThreadMessage() {
-        // добавляет сообщение в тред к предыдущему сообщению (для начала обсуждения), дополнительно принимает на вход
-    // parentMessageId для идентификации сообщения, под которым создается ветка обсуждения;
+    fun addThreadMessage(messageId: Int, childMessage: String, authorName: String) {
+        val message = ChildMessage(
+            messageId = messageId,
+            message = childMessage,
+            authorName = authorName,
+            childMessageId = Random.nextInt(0, Int.MAX_VALUE)
+        )
+        mutableListOfThreadMessage.add(message)
+    }
+
+    fun printChat(chat: Chat) {
+        chat.mutableListOfMessages.forEach() {
+            println("${it.message}")
+            val messageId = it.messageId
+            val groupedByMessageId: Map<Int, List<ChildMessage>> = mutableListOfThreadMessage.groupBy { it.messageId }
+            val childMessage: List<ChildMessage>? = groupedByMessageId[messageId]
+            childMessage?.forEach() { println("     ${it.message}") }
+        }
     }
 }
+
+open class Message(
+    val messageId: Int,
+    val message: String,
+    val authorName: String,
+)
+
+class ChildMessage(
+    messageId: Int,
+    message: String,
+    authorName: String,
+    val childMessageId: Int,
+) : Message(messageId, message, authorName)
+
