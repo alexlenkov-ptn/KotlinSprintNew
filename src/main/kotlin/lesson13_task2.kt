479ac1d1fa05d81dd9ea0ab2bcc339d7fc4c737b
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-06 21:39:09 +0300

    L13 T2

---
+++ src/main/kotlin/lesson13_task2.kt
@@ -0,0 +1,22 @@
+package lesson13_task2
+
+fun main() {
+    val person1 = TelephoneList_13_2("Vitaly", 89991599999, )
+    person1.printInfo()
+}
+
+class TelephoneList_13_2(
+    _name: String,
+    _number: Long,
+    _company: String? = null,
+) {
+    val name = _name
+    val number = _number
+    val company = _company?:null
+
+    fun printInfo() {
+        println("- Имя: $name \n" +
+                "- Номер: $number \n" +
+                "- Компания: ${ company ?: "не указано"}")
+    }
+}
