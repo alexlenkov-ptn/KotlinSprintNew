eacd451a9ea47695d2d12d0b5865c411081deba0
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-06 08:28:26 +0300

    L13T5 delete file

---
+++ src/main/kotlin/lesson13_task5.kt
@@ -0,0 +1,41 @@
+fun main() {
+    var userAnswer: String = ""
+    val listOfContact: List<TelephoneList_13_5> = listOf()
+
+    println("Введите номер телефона:")
+    val number = readln().toString()
+    try {
+        number.toLong()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("Введенное значение не является числом: $number", e)
+    }
+
+    println("Введите имя:")
+    val name = readln().toString().replaceFirstChar { it.uppercase() }
+    number.toLong()
+
+    println("Введите название компании:")
+    val company = readln()?.toString() ?: null
+}
+
+class TelephoneList_13_5(
+    _name: String,
+    _number: Long,
+    _company: String? = null,
+) {
+    val name = _name
+    val number = _number
+    val company = _company ?: null
+
+    fun printInfo() {
+        println(
+            "- Имя: $name \n" +
+                    "- Номер: $number \n" +
+                    "- Компания: ${if (company == null)
+                        "не указано"
+                    else
+                        company
+                    }"
+        )
+    }
+}
