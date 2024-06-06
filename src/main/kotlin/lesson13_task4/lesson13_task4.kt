34ed9bfde9233550bc778b9152cd0f79c857c419
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-06 22:22:31 +0300

    L13 T4

--- src/main/kotlin/lesson13_task4/lesson13_task4.kt
+++ src/main/kotlin/lesson13_task4/lesson13_task4.kt
@@ -4,17 +4,27 @@ const val ANSWER_NO = "нет"
 
 fun main() {
     var userAnswer: String = ""
-    val listOfContact: List<TelephoneList_13_4> = listOf()
+    val listOfContact: MutableList<TelephoneList_13_4> = mutableListOf()
 
     while (userAnswer != ANSWER_NO) {
-        println("Введите номер телефона:")
-        val name = readln().toString().capitalize()
-        val number = readln().toLongOrNull() ?: println("Вы не ввели номер телефона")
+        println("Введите имя:")
+        val name = readln().toString().replaceFirstChar { it.uppercase() }
+        var number: Long
+        do {
+            println("Введите номер телефона:")
+            val userNumberInput = readln()
+            number = userNumberInput?.toLongOrNull() ?: 0
+        } while (number == null)
+        println("Введите компанию:")
         val company = readln()?.toString() ?: null
-        val user: TelephoneList_13_4
+        val user: TelephoneList_13_4 = TelephoneList_13_4(name, number, company)
+
+
+        listOfContact.add(user(name, number, ))
         println("Продолжить заполнение базы? Да / Нет")
-        userAnswer = readln().toString().toLowerCase()
+        userAnswer = readln().toString().lowercase()
     }
+
 
     listOfContact.forEach {
         println(it)
@@ -35,6 +45,24 @@ class TelephoneList_13_4(
             "- Имя: $name \n" +
                     "- Номер: $number \n" +
                     "- Компания: $company"
+        )
+    }
+}
+
+class TelephoneList(
+    _name: String,
+    _number: Long,
+    _company: String? = null,
+) {
+    val name = _name
+    val number = _number
+    val company = _company
+
+    fun printInfo() {
+        println(
+            "- Имя: $name \n" +
+                    "- Номер: $number \n" +
+                    "- Компания: ${company ?: "не указано"}"
         )
     }
 }
