ff9936b2de625e2d10ad74f0f51a19c6f00e2e97
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-07 23:59:01 +0300

    L14 T4

---
+++ src/main/kotlin/lesson14_task4.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val fobos = PlanetSatellite("Fobos", false, true)
+    val deymos = PlanetSatellite("Deymos", false, true)
+    val mars: Planet = Planet("Mars", true, true, listOf(fobos, deymos))
+
+    println("Планета:")
+    mars.printName()
+    println("Спутники:")
+    mars.printNameOfPlanetSattelits()
+}
+
+abstract class CelestialBody(
+    val name: String,
+    val isAtmosphere: Boolean,
+    val areSuitableForPlanting: Boolean,
+) {
+    fun printName() {
+        println(name)
+    }
+}
+
+class Planet(
+    name: String,
+    isAtmosphere: Boolean,
+    areSuitableForPlanting: Boolean,
+    val listOfSatellits: List<PlanetSatellite>,
+) : CelestialBody(name, isAtmosphere, areSuitableForPlanting) {
+    fun printNameOfPlanetSattelits() {
+        listOfSatellits.forEach() {
+            println(it.name)
+        }
+    }
+}
+
+class PlanetSatellite(
+    name: String,
+    isAtmosphere: Boolean,
+    areSuitableForPlanting: Boolean,
+) : CelestialBody(name, isAtmosphere, areSuitableForPlanting)
