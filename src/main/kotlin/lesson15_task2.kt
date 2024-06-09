cb64467e1507c9441ff2c83277b8ae7fc347d44b
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-09 23:58:46 +0300

    L15 T2

--- src/main/kotlin/lesson15_task2.kt
+++ src/main/kotlin/lesson15_task2.kt
@@ -1,40 +1,35 @@
 fun main() {
-    val weatherServer: WeatherServer = WeatherServer()
+    val todayTemperature = Temperature(35)
+    val todayPrecipitationAmount = PrecipitationAmount(1)
+    val weatherServer = WeatherServer()
+    weatherServer.addStat(todayTemperature)
+    weatherServer.addStat(todayPrecipitationAmount)
 
+    println(weatherServer.mutableMapStat)
 }
 
-abstract class WeatherStationStats() {
-    val listT: MutableList<Temperature> = mutableListOf()
-    val listP: MutableList<PrecipitationAmount> = mutableListOf()
+abstract class WeatherStationStats()
+
+class Temperature(_count: Int) : WeatherStationStats() {
+    val count = _count
+}
+class PrecipitationAmount(_count: Int) : WeatherStationStats() {
+    val count = _count
 }
 
-class Temperature(val count: Int) : WeatherStationStats() {
-
+class WeatherServer() {
+    val mutableMapStat: MutableMap<String, Int> = mutableMapOf()
+    val temperatureName = "Температура"
+    val precipitationAmountName = "Количество осадков"
+    fun addStat(stats: WeatherStationStats) {
+        if (stats is Temperature) {
+            mutableMapStat[temperatureName] = stats.count
+        } else if (stats is PrecipitationAmount) {
+            mutableMapStat[precipitationAmountName] = stats.count
+        } else {
+            println("Мы не знаем что это такое")
+        }
+    }
 }
-
-class PrecipitationAmount(val count: Int) : WeatherStationStats() {
-
-}
-
-class WeatherServer(stat: WeatherStationStats) {
-    // сюда должны прилетать данные
-    val list: MutableList<WeatherStationStats> = mutableListOf()
-}
-
-/*
-Задача 2 к Уроку 15
-
-Необходимо разработать простую систему мониторинга погодных условий,
-которая будет собирать данные о температуре и количестве осадков.
-
-Создай абстрактный класс WeatherStationStats. Он будет служить родителем для классов-объектов погодных данных:
-Temperature и PrecipitationAmount.
-
-Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer.
-Метод отправки сообщения должен определять тип переданного сообщения и в зависимости от результата передавать
-соответствующие данные.
-
-Создай по одному объекту для разных типов погодных данных и протестируй работу программы.
- */
 
 
