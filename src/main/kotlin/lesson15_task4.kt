1633827d06ee2a719dbc3ece88503ec0c637d561
Alex Lenkov <@>
2024-06-10 21:32:49 +0300

    L15 T4 solution

--- src/main/kotlin/lesson15_task4.kt
+++ src/main/kotlin/lesson15_task4.kt
@@ -1,73 +1,33 @@
 fun main() {
-    val instrumentDrumSet = Instruments("Барабанная установка", 1)
-    val accessoriesForDrumSetSticks = AccessoriesForInstruments("Барабанные палочки", 10)
-    val accessoriesForDrumSetSnare = AccessoriesForInstruments("Малый барабан", 3)
-    val accessoriesForDrumSetHiHat = AccessoriesForInstruments("Хай-хэт", 9)
-    val accessoriesForDrumSetCrash = AccessoriesForInstruments("Крэш", 2)
+    val guitar = Instrument("Guitar", 5)
+    val drum = Instrument("Drum", 10)
+    val drumStick = Accessory("Drum Stick", 51)
+    val guitarStrings = Accessory("Guitar Strings", 10)
 
-    instrumentDrumSet.mutableListOfAccessoriesForInstruments.add(accessoriesForDrumSetSticks)
-    instrumentDrumSet.mutableListOfAccessoriesForInstruments.add(accessoriesForDrumSetSnare)
-    instrumentDrumSet.mutableListOfAccessoriesForInstruments.add(accessoriesForDrumSetHiHat)
-    instrumentDrumSet.mutableListOfAccessoriesForInstruments.add(accessoriesForDrumSetCrash)
+    guitar.searchAccessory()
+    guitarStrings.printInfo()
 
-    instrumentDrumSet.searchAccessories("Крэш")
+    drum.searchAccessory()
+    drumStick.printInfo()
+
 
 }
 
 interface Search {
-    fun searching() {
-        println("Выполняется поиск...")
+    fun searchAccessory() {
     }
 }
 
-abstract class Goods(_name: String,
-                     _count: Int,) {
-    val name = _name
-    val count = _count
-}
-
-class Instruments(name: String,
-                  count: Int) : Goods(name, count), Search {
-    val mutableListOfAccessoriesForInstruments: MutableList<AccessoriesForInstruments> = mutableListOf()
-
-    fun searchAccessories(nameAccessor: String) {
-        val accessor = mutableListOfAccessoriesForInstruments.map { it.name ==  nameAccessor}
-        searching()
-
-        println("Комплектующие для инструмента: ${name}:\n" +
-                "${mutableListOfAccessoriesForInstruments.map { it.name ==  nameAccessor}}: " +
-                "${mutableListOfAccessoriesForInstruments.map { it.count }}")
+class Instrument(val name: String,
+                 val count: Int) : Search {
+    override fun searchAccessory() {
+        println("Выполняется поиск для инструмента $name...")
     }
 }
 
-class AccessoriesForInstruments(name: String,
-                                count: Int) : Goods(name, count) {
+class Accessory(val name: String,
+                val count: Int) {
+    fun printInfo() = println("$name: $count шт.")
 }
-
-
-
-/*
-Задача 4 к Уроку 15
-
-На сайте музыкального магазина есть товары двух категорий:
-инструменты и комплектующие к ним.
-
-Все товары имеют название и количество единиц на складе.
-Для инструментов есть возможность поиска соответствующих комплектующих.
-
-Опиши классы для категорий товаров. Имитируй процесс поиска текстовым сообщением «Выполняется поиск».
-Для выноса логики поиска используй интерфейс.
- */
-
-/*
-Будет 2 класса - инструменты, комплектующие к инструментам
-Получается нам нужна:
-Коллекция для инструментов
-Map для комплектующих
-
-Инструменты и комплектующие имеют: название и количество единиц на складе
-
-Логика поиска - это интерфейс
- */
 
 
