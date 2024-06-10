7d48234074e203ffda25a05e8d05cb9ba3ed5c96
Alex Lenkov <@>
2024-06-10 22:17:45 +0300

    L15 T4 create

---
+++ src/main/kotlin/lesson15_task4.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val guitar = Instrument("Guitar", 5)
+    val drum = Instrument("Drum", 10)
+    val drumStick = Accessory("Drum Stick", 51)
+    val guitarStrings = Accessory("Guitar Strings", 10)
+
+    guitar.searchAccessory()
+    guitarStrings.printInfo()
+
+    drum.searchAccessory()
+    drumStick.printInfo()
+
+}
+
+interface Search {
+    fun searchAccessory() {
+    }
+}
+
+class Instrument(val name: String,
+                 val count: Int) : Search {
+    override fun searchAccessory() {
+        println("Выполняется поиск для инструмента $name...")
+    }
+}
+
+class Accessory(val name: String,
+                val count: Int) {
+    fun printInfo() = println("$name: $count шт.")
+}
