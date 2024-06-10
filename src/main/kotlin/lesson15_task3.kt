58171036364e0614d7040bf81409e092e6eed790
Alex Lenkov <@>
2024-06-10 21:16:44 +0300

    L15 T3 corrected solution

--- src/main/kotlin/lesson15_task3.kt
+++ src/main/kotlin/lesson15_task3.kt
@@ -14,7 +14,6 @@ fun main() {
     adm.deleteForumUser(user)
 }
 
-
 abstract class User(val name: String) {
     fun readMessage() = println("Пользователь <$name> читает сообщение")
     fun printForumMessage(message: String) = println(message)
