e92ea0896126f17eb8973c2b4d0a1bb7f3a6567b
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-13 23:01:12 +0300

    L16 T5 solution

--- src/main/kotlin/lesson16_task5.kt
+++ src/main/kotlin/lesson16_task5.kt
@@ -1,3 +1,22 @@
 fun main() {
 
 }
+
+class Player(
+    protected val name: String,
+    protected var health: Int,
+    protected val damage: Int,
+) {
+
+    fun getDamage (damage: Int) {
+        health = health - damage
+    }
+
+    fun getHeal (heal: Int) {
+        health = health + heal
+    }
+
+    private fun healthIsOver() {
+
+    }
+}
