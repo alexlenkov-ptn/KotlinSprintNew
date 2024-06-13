5c4785d539189fe5c56d8b5148812ac13b5cc727
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-13 23:14:54 +0300

    L16 T5 solution

--- src/main/kotlin/lesson16_task5.kt
+++ src/main/kotlin/lesson16_task5.kt
@@ -1,22 +1,51 @@
 fun main() {
-
+    val player = Player("player", 100, 4)
+    player.getDamage(50)
+    player.getConditionPlayer()
+    println()
+    player.getHeal(100)
+    player.getConditionPlayer()
+    println()
+    player.getDamage(50)
+    player.getConditionPlayer()
+    println()
+    player.getHeal(100)
+    player.getConditionPlayer()
+    println()
+    player.getDamage(200)
+    player.getConditionPlayer()
+    println()
+    player.getDamage(200)
 }
 
 class Player(
     protected val name: String,
     protected var health: Int,
-    protected val damage: Int,
+    protected var damage: Int,
 ) {
 
     fun getDamage (damage: Int) {
-        health = health - damage
+        if (health > 0) health = health - damage
+        else println("Урон по персонажу не работает. Игрок мертв")
+        healthIsOver(health)
     }
 
     fun getHeal (heal: Int) {
-        health = health + heal
+        if (health > 0) health = health + heal
+        else println("Лечение не работает. Игрок мертв")
+        healthIsOver(health)
     }
 
-    private fun healthIsOver() {
+    private fun healthIsOver(health: Int) {
+        if (health <= 0) {
+            this.health = 0
+            damage = 0
+        }
+    }
 
+    fun getConditionPlayer() {
+        println("Состояние игрока $name: \n" +
+                "Здоровье: $health\n" +
+                "Наносимый урон: $damage")
     }
 }
