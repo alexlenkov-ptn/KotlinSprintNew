979d74e1723e1ff59b129b00c7237ed6d4524e5c
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-14 23:16:17 +0300

    L17 T1 solution

---
+++ src/main/kotlin/lesson17_task1.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+}
+
+class Quiz() {
+    val question: String
+        get() = ""
+
+    var answer: String = ""
+        get() = ""
+        set(value: String) {
+            field = value
+        }
+}
