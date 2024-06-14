1564808a6b45af31b64ea474f64eebb06c076551
Alex Lenkov <@>
2024-06-14 21:10:18 +0300

    L16 T3 solution

--- src/main/kotlin/lesson16_task3.kt
+++ src/main/kotlin/lesson16_task3.kt
@@ -1,18 +1,15 @@
-
-
 fun main() {
     val user = User("John", "0000")
-    println(checkPassword("0000", user))
+    println(user.checkPassword("0000"))
 }
 
 class User(
     private val login: String,
     private val password: String,
 ) {
-    fun getPassword() = password
-}
 
-fun checkPassword(password: String, user: User) : Boolean {
-    if(password == user.getPassword()) return true
-    else return false
+    fun checkPassword(password: String): Boolean {
+        if (password == this.password) return true
+        else return false
+    }
 }
