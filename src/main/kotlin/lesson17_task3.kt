0d14c1739c8a5271031a9c446e77c5cd65f1a811
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-15 18:10:33 +0300

    L17 T3 solution

---
+++ src/main/kotlin/lesson17_task3.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val folderSecret = Folder("folder secret", 2, true)
+    val folder = Folder("folder", 5, false)
+    folderSecret.name
+    folder.name
+}
+
+class Folder(name: String,
+    numberFiles: Int,
+    isSecret: Boolean,) {
+
+    val name: String = name
+        get() = if (isSecret == true) println("Скрытая папка\n" +
+                "Количество файлов - 0").toString()
+        else println("Папка <$field>\n" +
+                "Количество файлов - ${numberFiles}").toString()
+
+    private val numberFiles = numberFiles
+    private val isSecret = isSecret
+
+}
