1eef7fd9f6ab9ac7b5297687e0767e412c6cd582
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-23 17:49:25 +0300

    L19 T2

---
+++ src/main/kotlin/lesson19-task2.kt
@@ -0,0 +1,38 @@
+fun main() {
+    val product = Product("Брюки", 1, Category.CLOTHES)
+    product.showProductInfo()
+}
+
+enum class Category() {
+    CLOTHES {
+        override fun getName(): String {
+            return "Одежда"
+        }
+    },
+    CHANCELLERY {
+        override fun getName(): String {
+            return "Канцелярские товары"
+        }
+    },
+    DIFFERENT {
+        override fun getName(): String {
+            return "Разное"
+        }
+    };
+
+    abstract fun getName(): String
+}
+
+class Product(
+    val name: String,
+    val id: Int,
+    val category: Category,
+) {
+    fun showProductInfo() {
+        println(
+            "Товар: $name\n" +
+                    "Идентификатор товара: $id\n" +
+                    "Категория товар: ${category.getName()}"
+        )
+    }
+}
