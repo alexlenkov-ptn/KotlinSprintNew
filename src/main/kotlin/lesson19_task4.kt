5cb63a940f22dadded36f9b6ceb3745631fb053c
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-24 23:42:55 +0300

    L19 T4 corrected

--- src/main/kotlin/lesson19_task4.kt
+++ src/main/kotlin/lesson19_task4.kt
@@ -1,20 +1,18 @@
-import kotlin.math.tan
-
 fun main() {
     val tank = Tank()
     tank.shot()
     println()
-    tank.reload(Cartridges.BLUE)
+    tank.reload(Cartridge.BLUE)
     tank.shot()
     println()
-    tank.reload(Cartridges.RED)
+    tank.reload(Cartridge.RED)
     tank.shot()
     println()
-    tank.reload(Cartridges.GREEN)
+    tank.reload(Cartridge.GREEN)
     tank.shot()
 }
 
-enum class Cartridges(val damage: Int?) {
+enum class Cartridge(val damage: Int?) {
     BLUE(5),
     GREEN(10),
     RED(20),
@@ -22,9 +20,9 @@ enum class Cartridges(val damage: Int?) {
 }
 
 class Tank() {
-    private var cartridges: Cartridges = Cartridges.NONE
+    private var cartridges: Cartridge = Cartridge.NONE
 
-    fun reload(cartridges: Cartridges) {
+    fun reload(cartridges: Cartridge) {
         this.cartridges = cartridges
         println("Танк перевооружён на:\n" +
                 "тип патронов - ${cartridges.name};\n" +
@@ -32,7 +30,7 @@ class Tank() {
     }
 
     fun shot() {
-        if (cartridges == Cartridges.NONE) {
+        if (cartridges == Cartridge.NONE) {
             println("Сначала зарядите танк")
         }else {
             println("Танк выстрелил. Урон - ${cartridges.damage} ед.")
