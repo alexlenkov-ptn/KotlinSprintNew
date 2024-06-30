ffe05b2fdf84af9171f7c8bffc485f41d1101303
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-30 18:36:37 +0300

    L20 T4 corrected

--- src/main/kotlin/lesson20_task4.kt
+++ src/main/kotlin/lesson20_task4.kt
@@ -1,9 +1,11 @@
 fun main() {
-    val list: List<String> = listOf("первая строка", "вторая строка", "третья строка", "четвертая строка",
-        "пятая строка")
-    val lambda: (String) -> String = {"Нажат элемент <$it>"}
+    val list: List<String> = listOf(
+        "первая строка", "вторая строка", "третья строка", "четвертая строка",
+        "пятая строка"
+    )
+    val lambda: (String) -> String = { "Нажат элемент <$it>" }
     list.map {
-        val index = list.indexOf(it)+1
+        val index = list.indexOf(it) + 1
         if (index % 2 == 0) println(lambda(it))
     }
 }
