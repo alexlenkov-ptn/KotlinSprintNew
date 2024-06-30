252358030ed4f54aac45ad13fa9405fc7647c5f1
Alex Lenkov <alex.lenkov123@outlook.com>
2024-06-30 22:20:15 +0300

    L20 T5

--- src/main/kotlin/lesson20_task5.kt
+++ src/main/kotlin/lesson20_task5.kt
@@ -1,21 +1,29 @@
+import kotlin.random.Random
+
 fun main() {
     val robot = Robot()
-    var string = "I'm a robot"
-    robot.say(string)
-    string = setModifier(string)
-    robot.say(string)
+    println(robot.say())
+    println(robot.setModifier())
+    println(robot.say())
 }
 
 class Robot() {
-    fun say(string: String) {
-        println(string)
+    fun say(): String {
+        return randomStringList()
     }
-}
 
-fun setModifier(
-    string: String,
-    invertor: (String) -> String = { it.reversed() }
-): String {
-    val invertorString = invertor(string)
-    return invertorString
+    private var phrase1 = "Я робот"
+    private var phrase2 = "Я готов выполнять команды"
+    private var phrase3 = "Я умный"
+    private var phrase4 = "Я думаю наперёд"
+    private var phrase5 = "Меня программирует человек"
+    private val list: List<String> = listOf(phrase1, phrase2, phrase3, phrase4, phrase5)
+
+    private val randomStringList: () -> String = { list[Random.nextInt(0, list.size - 1)] }
+
+    fun setModifier(
+        invertor: (String) -> String = { it.reversed() }
+    ) {
+        val invertorString = invertor(randomStringList.toString())
+    }
 }
