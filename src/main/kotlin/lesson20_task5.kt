2d8dff67a33fe91e1d936456fdb6d80e5288634c
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-06 13:17:04 +0300

    L20 T5

--- src/main/kotlin/lesson20_task5.kt
+++ src/main/kotlin/lesson20_task5.kt
@@ -15,7 +15,6 @@ class Robot() {
     private var phrase4 = "Я думаю наперёд"
     private var phrase5 = "Меня программирует человек"
     private val list: List<String> = listOf(phrase1, phrase2, phrase3, phrase4, phrase5)
-
     private val randomStringList: () -> String = { list[Random.nextInt(0, list.size)] }
     private var modifier = randomStringList
 
@@ -28,6 +27,6 @@ class Robot() {
     fun setModifier(
         invertor: (String) -> String = { it.reversed() }
     ) {
-
+        modifier = { invertor(randomStringList()) }
     }
 }
