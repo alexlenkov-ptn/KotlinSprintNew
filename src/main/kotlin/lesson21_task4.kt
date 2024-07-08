8bf333a848011961150f8841b9fbe70fe5e5a9ae
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-08 05:59:23 +0300

    L21 T4

--- src/main/kotlin/lesson21_task4.kt
+++ src/main/kotlin/lesson21_task4.kt
@@ -2,10 +2,10 @@ import java.io.File
 
 fun main() {
     val file = File("1.txt")
-    file.writeTextInLowerCase("ТеКСТ")
+    file.writeTextInLowerCase(" ТеКСТ1 ")
     println(file.readText())
 }
 
 fun File.writeTextInLowerCase(text: String) {
-    this.writeText(text.lowercase())
+    this.writeText(text.lowercase() + this.readText())
 }
