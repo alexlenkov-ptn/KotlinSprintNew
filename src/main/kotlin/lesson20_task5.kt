e9941562790b48adcffbaf9d05b38c1609af75bd
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-08 22:13:33 +0300

    L20 T5

--- src/main/kotlin/lesson20_task5.kt
+++ src/main/kotlin/lesson20_task5.kt
@@ -1,9 +1,10 @@
+import javax.lang.model.element.Modifier
 import kotlin.random.Random
 
 fun main() {
     val robot = Robot()
     robot.say()
-    robot.setModifier()
+    robot.setModifier { str: String -> str.reversed() }
     robot.say()
 }
 
@@ -15,18 +16,17 @@ class Robot() {
     private var phrase4 = "Я думаю наперёд"
     private var phrase5 = "Меня программирует человек"
     private val list: List<String> = listOf(phrase1, phrase2, phrase3, phrase4, phrase5)
-    private val randomStringList: () -> String = { list[Random.nextInt(0, list.size)] }
-    private var modifier = randomStringList
+
+    private var modifier: (String) -> String = { it }
 
     fun say(
-        printPhrase: () -> Unit = {println(modifier()).toString()}
     ) {
-        printPhrase()
+        println(modifier(list.random()))
     }
 
     fun setModifier(
-        invertor: (String) -> String = { it.reversed() }
+        modifier: (String) -> String
     ) {
-        modifier = { invertor(randomStringList()) }
+        this.modifier = modifier
     }
 }
