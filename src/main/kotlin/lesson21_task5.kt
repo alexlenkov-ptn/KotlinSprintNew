387874d57d6379cfd336156542d227efecfa8cb7
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-10 10:17:26 +0300

    L21 T5

--- src/main/kotlin/lesson21_task5.kt
+++ src/main/kotlin/lesson21_task5.kt
@@ -3,14 +3,15 @@ fun main() {
     println(player.mapExperienceAndSkills.maxCategory())
 }
 
-class Player(val experience: Int) {
-    val mapExperienceAndSkills: Map<String, Int> = mapOf(
+class Player(private val experience: Int) {
+    var mapExperienceAndSkills: MutableMap<String, Int> = mutableMapOf(
         "Вампиризм" to 100,
         "Призыв нежити" to 200,
         "Панацея" to 300,
         "Призыв стража смерти" to 500,
         "Мир призраков" to 500
-    )
+    ).filterValues { it <= experience }.toMutableMap()
+
 }
 
 fun Map<String, Int>.maxCategory(): String? = maxByOrNull { it.value }?.key
