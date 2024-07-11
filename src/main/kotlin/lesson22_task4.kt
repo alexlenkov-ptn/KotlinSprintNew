62044ec9e57bff4098278af91b038187c2970a90
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-11 06:54:23 +0300

    L22 T4

--- src/main/kotlin/lesson22_task4.kt
+++ src/main/kotlin/lesson22_task4.kt
@@ -1,18 +1,23 @@
 fun main() {
-    var mainScreenState = MainScreenState()
-    println(mainScreenState.isLoading)
-    mainScreenState = loadData(mainScreenState)
-    println(mainScreenState.isLoading)
-    mainScreenState = loadData(mainScreenState)
-    println(mainScreenState.isLoading)
-
+    val mainScreenViewModel = MainScreenViewModel()
+    println(mainScreenViewModel.mainScreenState.isLoading)
+    mainScreenViewModel.loadData()
+    println(mainScreenViewModel.mainScreenState.isLoading)
+    mainScreenViewModel.loadData()
+    println(mainScreenViewModel.mainScreenState.isLoading)
 }
 
-data class MainScreenState (
-        val data: String = "",
-        val isLoading : Boolean = false,
-        ) {}
+class MainScreenViewModel(
+    var mainScreenState: MainScreenState = MainScreenState()
+) {
 
-fun loadData(mainScreenState: MainScreenState) : MainScreenState {
-    return mainScreenState.copy(isLoading = !mainScreenState.isLoading)
+    data class MainScreenState(
+        val data: String = "",
+        val isLoading: Boolean = false,
+    )
+
+    fun loadData(): MainScreenState {
+        mainScreenState = mainScreenState.copy(isLoading = !mainScreenState.isLoading)
+        return mainScreenState
+    }
 }
