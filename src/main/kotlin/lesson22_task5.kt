632765694fa498ab3621b0f7c894dfe177252857
Alex Lenkov <alex.lenkov123@outlook.com>
2024-07-11 07:11:19 +0300

    L22 T5

--- src/main/kotlin/lesson22_task5.kt
+++ src/main/kotlin/lesson22_task5.kt
@@ -5,15 +5,17 @@ fun main() {
         "11.05.2078",
         4.36
     )
-    val (name, description, date, distance) = alphaCentauri
+    val name = alphaCentauri.component1()
+    val description = alphaCentauri.component2()
+    val date = alphaCentauri.component3()
+    val distance = alphaCentauri.component4()
     println(
         "Название места или события: $name\n" +
-                "Описание места или события: $description\n" +
+                "Описание мета или события: $description\n" +
                 "Дата и время события: $date\n" +
                 "Расстояние места или события от Земли в световых годах: $distance "
     )
 }
-
 data class GalacticGuide(
     val name: String,
     val description: String,
